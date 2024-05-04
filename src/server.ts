3f93ad4dbc64ce5e76fe131442eeb238a3008c2d
Leandro Santos <l.santos@chimerainformatica.com>
2024-05-04 15:51:11 +0200

    fix: add type module in package

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,8 @@ app.get('/videos/:id', () => {
   return { type: 'test funziona' }
 })
 
+//Commento
+
 app.listen({
   host: '0.0.0.0',
   port: process.env.PORT ? Number(process.env.PORT) : 3000,
